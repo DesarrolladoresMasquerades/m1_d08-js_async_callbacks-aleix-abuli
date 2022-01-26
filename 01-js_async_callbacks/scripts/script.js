ef9b1056e81562d3700acfc03107e1e7f9dc9595
Aleix Abulí <aleixabuli088@gmail.com>
2022-01-26 11:48:42 +0100

    finished

--- 01-js_async_callbacks/scripts/script.js
+++ 01-js_async_callbacks/scripts/script.js
@@ -1,1 +1,70 @@
 console.log("JS Loaded")
+
+const h1Tag = document.getElementsByTagName("h1")[0];
+/*
+setTimeout(
+    ()=> {
+        console.log("I am a pizza")
+        h1Tag.innerText += "I like pizza"
+    },
+    (Math.random() * 5 * 1000) // this is real-time in milliseconds
+)
+
+setTimeout(
+    ()=> {
+        console.log("I am a polpetta al ragu")
+        h1Tag.innerText += " and I also like polpetta"
+    },
+    (Math.random() * 5 * 1000) // this is real-time in milliseconds
+)
+
+setTimeout(
+    ()=> {
+        console.log("I like steak")
+        h1Tag.innerText += " and I also like steak"
+    },
+    (Math.random() * 5 * 1000) // this is real-time in milliseconds
+)
+*/
+
+
+
+function onButton1Click(){
+    console.log("Button 1 clicked")
+    setTimeout(
+        ()=>h1Tag.innerText += " and I also like steak",
+        Math.random() * 5 * 1000
+    )
+}
+
+function onButton2Click(){
+    console.log("Button 2 clicked")
+    setTimeout(
+        ()=>h1Tag.innerText += " and I also like polpetta",
+        Math.random() * 5 * 1000
+    )
+}
+
+function onButton3Click(){
+    console.log("Button 3 clicked")
+    setTimeout(
+        ()=>h1Tag.innerText += "I like pizza",
+        Math.random() * 5 * 1000
+    )
+}
+
+
+
+// ----------- timer ------------
+
+const counter = document.getElementById("counter")
+
+const myInterval = setInterval(
+    ()=> {counter.innerText = parseInt(counter.innerText) + 1
+        if(counter.innerText === "3"){
+            clearInterval(myInterval)}
+        }, 1 * 1000
+)
+
+
+const stopCounter = () => clearInterval(myInterval)  // this is the only way to stop the clock, otherwise it will go on forever
--- exercises/01-exe-js_async_callbacks/scripts/script.js
+++ exercises/01-exe-js_async_callbacks/scripts/script.js
@@ -8,6 +8,12 @@ function print() {
   }
   
   print(); 
+
+  // Console logs 2 after a second. 
+  // Console.logs 3 at the moment you open the page, without waiting.
+  // Console.logs 4 after all the above are done.
+  // WRONG!!!!!!!! Console logs 1 & 4 first bc they don't have a timeout set,
+  // then it console logs 3 bc timeout is set to 0 and after 1 sec it logs 2
   
   // ------------------------------------------------------------------------
   
@@ -17,4 +23,21 @@ function print() {
   // - For every number divisible by 5 it prints a BANG!!
   // - Forevery number divisible for 3 and 5 it prints BOOM BANG!! 
   
-  
+  function blast() {
+    let counter = 1;
+
+    setInterval(
+      ()=>{
+        for(let i=1; i<=15; i++){
+          if ((i%3===0) && (i%5===0)) counter = "BOOM BANG!!";
+          else if (i%3===0) counter = "BOOM!!";
+          else if (i%5===0) counter = "BANG!!";
+          else counter = i;
+          console.log(counter);
+        }
+      }
+      , 1*1000
+    )
+  }
+
+  blast();
